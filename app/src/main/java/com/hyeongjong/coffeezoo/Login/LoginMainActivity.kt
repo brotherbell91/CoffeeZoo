b011310218fb1699a1c298a446a02a63ab57464a
이형종 <hj36879@naver.com>
2022-03-01 21:33:03 +0900

    카카오, 네이버, 구글, 애플 리사이즈 버튼 삽입

--- app/src/main/java/com/hyeongjong/coffeezoo/Login/LoginMainActivity.kt
+++ app/src/main/java/com/hyeongjong/coffeezoo/Login/LoginMainActivity.kt
@@ -1,12 +1,48 @@
 package com.hyeongjong.coffeezoo.Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.hyeongjong.coffeezoo.MainActivity
 import com.hyeongjong.coffeezoo.R
+import kotlinx.android.synthetic.main.activity_login_main.*
 
 class LoginMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_main)
+
+//        로그인 버튼 구현
+        btnLogin.setOnClickListener {
+            var inputId = edtInputId.text.toString()
+            var inputPw = edtInputPw.text.toString()
+
+//            맞다면
+            if (inputId == "admin" && inputPw == "qwer") {
+
+                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+
+                btnLogin.setOnClickListener {
+
+                    val myIntent = Intent(this,MainActivity::class.java)
+
+                    startActivity(myIntent)
+
+                }
+
+            }
+//            아이디가 틀리다면
+            else if(inputId != "admin") {
+
+                Toast.makeText(this, "잘못된 아이디입니다.", Toast.LENGTH_SHORT).show()
+            }
+//            아이디는 맞고 비밀번호가 틀리다면
+            else {
+
+                Toast.makeText(this, "잘못된 비밀번호입니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
