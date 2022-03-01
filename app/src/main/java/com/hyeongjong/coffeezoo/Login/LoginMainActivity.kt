4dc7bd7f16715da14578aa88af75590baaa3b67f
이형종 <hj36879@naver.com>
2022-03-01 19:40:05 +0900

    Initial commit

---
+++ app/src/main/java/com/hyeongjong/coffeezoo/Login/LoginMainActivity.kt
@@ -0,0 +1,12 @@
+package com.hyeongjong.coffeezoo.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyeongjong.coffeezoo.R
+
+class LoginMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_main)
+    }
+}
