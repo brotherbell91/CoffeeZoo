a5d114aa303d5d065cc718cb19472c0f712a70ff
이형종 <hj36879@naver.com>
2022-03-05 20:12:21 +0900

    ViewCafeDetailActivity 생성 및 activity_view_cafe_detail 작성(id 연결 x)

---
+++ app/src/main/java/com/hyeongjong/coffeezoo/ViewCafeDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.hyeongjong.coffeezoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewCafeDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_cafe_detail)
+    }
+}
