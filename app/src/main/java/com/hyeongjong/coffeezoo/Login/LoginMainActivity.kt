0eb0be80e3d1676f808cf9fc602de759fd3fa2a3
이형종 <hj36879@naver.com>
2022-07-15 12:35:35 +0900

    소셜 로그인시 로그인 Toast 메시지 추가

--- app/src/main/java/com/hyeongjong/coffeezoo/Login/LoginMainActivity.kt
+++ app/src/main/java/com/hyeongjong/coffeezoo/Login/LoginMainActivity.kt
@@ -94,5 +94,7 @@ class LoginMainActivity : BaseActivity() {
         val myIntent = Intent(this,MainActivity::class.java)
         startActivity(myIntent)
 
+        Toast.makeText(this, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
+
     }
 }
