2fa977b635ce5e3b5625aaff9840ceb768ad0fed
이형종 <hj36879@naver.com>
2022-08-17 18:56:44 +0900

    ReviewData.kt 데이터 항목 추가 (수정2)  - num Double로 변경

--- app/src/main/java/com/hyeongjong/coffeezoo/datas/ReviewData.kt
+++ app/src/main/java/com/hyeongjong/coffeezoo/datas/ReviewData.kt
@@ -3,13 +3,13 @@ package com.hyeongjong.coffeezoo.datas
 import java.io.Serializable
 
 class ReviewData(
-//    val cafeName : String = "cafeName",
+    val cafeName : String = "cafeName",
     val comment : String = "comment",
-//    val date : String = "2022-01-01 07:07:07",
+    val date : String = "2022-01-01 07:07:07",
     val id : String = "apple@naver.com",
     val image : String = "image",
-//    val nick : String = "사과",
-//    val num : String = "num",
+    val nick : String = "사과",
+    val num : Double = 1.0,
 ) : Serializable {
 
 //    companion object {
