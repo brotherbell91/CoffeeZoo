d0de5c1f996b075c61d2b35672223e51471fe7fe
이형종 <hj36879@naver.com>
2022-08-17 18:37:16 +0900

    ReviewData.kt 데이터 항목 추가

--- app/src/main/java/com/hyeongjong/coffeezoo/datas/ReviewData.kt
+++ app/src/main/java/com/hyeongjong/coffeezoo/datas/ReviewData.kt
@@ -3,8 +3,13 @@ package com.hyeongjong.coffeezoo.datas
 import java.io.Serializable
 
 class ReviewData(
-    val image : String = "image",
+    val cafeName : String = "cafeName",
     val comment : String = "comment",
+    val date : String = "2022-01-01 07:07:07",
+    val id : String = "apple@naver.com",
+    val image : String = "image",
+    val nick : String = "사과",
+    val num : String = "num",
 ) : Serializable {
 
 //    companion object {
