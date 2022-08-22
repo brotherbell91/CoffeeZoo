8aa769de871d0f8e6a74cd8022c670a6d5894a55
이형종 <hj36879@naver.com>
2022-08-22 12:20:53 +0900

    회원가입 Firebase Auth 연결  - gradle 설치

--- app/src/main/java/com/hyeongjong/coffeezoo/Login/SignUpActivity.kt
+++ app/src/main/java/com/hyeongjong/coffeezoo/Login/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.hyeongjong.coffeezoo.Login
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.hyeongjong.coffeezoo.BaseActivity
 import com.hyeongjong.coffeezoo.R
 import com.hyeongjong.coffeezoo.databinding.ActivitySignUpBinding
@@ -10,17 +11,33 @@ class SignUpActivity : BaseActivity() {
 
     lateinit var binding : ActivitySignUpBinding
 
+    var auth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
 
+        auth = FirebaseAuth.getInstance()
+
+        setupEvents()
+        setValues()
+
+
     }
 
     override fun setupEvents() {
+
+        binding.btnSignUp.setOnClickListener {
+
+            
+
+        }
 
     }
 
     override fun setValues() {
 
     }
+
+
 }
